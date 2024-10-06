241571053cbf92f6414a019188cabd78e9ddf938
Brayann Benavides <brayannbegu11@gmail.com>
2024-10-06 18:51:01 -0400

    Bulding the general UI for one product

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,35 @@
+import Colors from '@/src/constants/Colors';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import products from '@/assets/data/products';
+
+const product = products[0]
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: product.image}} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10
+  },
+  title: {
+    fontSize: 18, 
+    fontWeight: '600',
+    marginVertical: 10
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold'
+  },
+  image:{
+    width: '100%',
+    aspectRatio: 1
+  }
+})
